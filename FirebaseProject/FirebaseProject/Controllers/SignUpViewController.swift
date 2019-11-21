55be9dfbb52e9ee38fe5c969028bf6bd142a6026
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-11-21 15:19:23 -0500

    login vc ui

---
+++ FirebaseProject/FirebaseProject/Controllers/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  FirebaseProject
+//
+//  Created by Anthony Gonzalez on 11/21/19.
+//  Copyright © 2019 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
